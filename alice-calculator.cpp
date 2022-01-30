7b29bd7a6122f0287b8eebe369e9a5b12144e17f
ppranav06 <pranavkrishnap856@gmail.com>
2022-01-30 23:21:03 +0530

    Improved to evaluate EXPRESSIONS directly (2 eltities

--- alice-calculator.cpp
+++ alice-calculator.cpp
@@ -1,57 +1,28 @@
-//Alice Calculator++
+// Alice Calculator
+
 #include<iostream>
 #include<iomanip>
 using namespace std;
-long double calculations (float a, float b, char op){
-    cout<<"Enter the first number: ";
-    cin>>a;
-    cout<<"Enter the second number: ";
-    cin>>b;
-    cout<<"Enter the operator: ";
-    cin>>op;
-    switch(op){
-        case '+':
-        case 'a':
-            cout<<endl<<a<<" + "<<b<<" = "<<a+b<<endl;
-            break;
-        case '-':
-        case 's':
-            cout<<endl<<a<<" - "<<b<<" = "<<a-b<<endl;
-            break;
-        case '*':
-        case 'm':
-            cout<<endl<<a<<" * "<<b<<" = "<<a*b<<endl;
-            break;
-        case '/':
-        case 'd':
-            cout<<endl<<a<<" / "<<b<<" = "<<a/b<<endl;
-            break;
-        default:
-            cout<<"Invalid operator"<<endl;
-    }
-    return 0;
+long double calculate(long double a, char op, long double b){
+  switch(op){
+      case '+': cout<<fixed<<a+b<<endl; break;
+      case '-': cout<<fixed<<a-b<<endl; break;
+      case '*': cout<<fixed<<a*b<<endl; break;
+      case '/': cout<<fixed<<a/b<<endl; break;
+      default: cout<<"Invalid operator"<<endl;
+  }
+  return 0;
 }
-int main(){
-    system("clear");
-    cout<<"Alice Calculator++"<<endl;
-    float n1,n2;
-    char op, option;
-    beginning:
-    cout<<endl<<"Choices:"<<endl<<"1 = Basic operations\nq/e = Exit"<<endl;
-    cout<<"Enter your choice: ";
-    cin>>option;
 
-    if (option=='1'){
-    calculations(n1,n2,op);
-    goto beginning;
-    }
-    else if (option=='e' | option=='E' | option=='q' | option=='Q'){
-        cout<<"Exiting..."<<endl;
-        return 0;
-    }
-    else{
-        cout<<"Invalid option"<<endl;
-        goto beginning;
-    }
-    return 0;
+int main(){
+  cout<<"Alice Calculator"<<endl;
+  cout<<"Enter the expression"<<endl;
+
+  // Declaring a and b variables along with the operator
+  long double a,b;
+  char op;
+
+  cin>>a>>op>>b;
+  calculate(a,op,b);
+  return 0;
 }
